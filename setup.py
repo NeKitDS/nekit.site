4d696ecbc686dbbdfaf4d826bc2cd004dbd6f21d
Nikita <43587167+NeKitDS@users.noreply.github.com>
2019-11-04 14:40:53 +0300

    Update the version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     project_urls={
         "Issue tracker": "https://github.com/NeKitDS/nekit.site/issues",
     },
-    version='0.1.3a3',
+    version='0.1.3a4',
     packages=[
         'nekit_site', 'nekit_site.ext'
     ],
