df5a3c1e0ade38c4e27cdd1d45e47e80565ccd60
Nikita Tikhonov <43587167+NeKitDS@users.noreply.github.com>
2020-04-01 08:22:16 +0300

    Add `run_async`.

--- nekit_site/core.py
+++ nekit_site/core.py
@@ -1,9 +1,15 @@
+from aiohttp import web
 import gd
 
 from .constants import port, routes
 from .ext import *
 
 app = gd.server.create_app()
+
+
+async def run_async() -> None:
+    app.add_routes(routes)
+    await web._run_app(app, port=port)
 
 
 def run() -> None:
