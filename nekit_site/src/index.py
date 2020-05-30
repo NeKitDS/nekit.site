c8604d63ee2823011ff9fcad924aa74773933230
Nikita Tikhonov <nekitguypro@gmail.com>
2020-05-31 02:53:46 +0300

    [0.4.0] Overall site improvements and additions.

--- nekit_site/src/index.py
+++ nekit_site/src/index.py
@@ -4,8 +4,26 @@ __all__ = ("handle_index",)
 
 template = env.get_template("index.html")
 
+projects = [
+    {
+        "title": "gd.py",
+        "info": (
+            "gd.py is an API wrapper for Geometry Dash. "
+            "It covers server, client and memory interaction in the game."
+        ),
+        "link": "https://github.com/NeKitDS/gd.py",
+        "link_name": "GitHub",
+    },
+    {
+        "title": "nekit.site",
+        "info": "This is the project that focuses on creating this website.",
+        "link": "https://github.com/NeKitDS/nekit.site",
+        "link_name": "GitHub",
+    }
+]
+
 
 @routes.get("/")
 async def handle_index(request: web.Request) -> web.Response:
-    text = await template.render_async()
+    text = await template.render_async(projects=projects)
     return html_resp(text=text)
